5f17296820e30f023672ace638ef3ee8655b4c95
Ganiyu Idris <Golamide27@gmail.com>
2022-12-23 08:47:54 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,26 @@ const isCopyDisabled = computed(() => {
 
 // A function to translate input text from english to danish
 const translate = () => {
+  var _form = $('#translationform')[0];
+  var paradata = new FormData(_form);
 
+  $.ajax({
+      type: "POST",
+      url: "{{url_for( 'mtDanish') }}",
+      data: paradata,
+      dataType: 'script',
+      processData: false,
+      contentType: false,
+      cache: false,
+      timeout: 800000,
+      success: function (res) {
+
+          $('#outputparatext').text(res);
+      },
+      error: function (e) {
+          console.log("ERROR : ", e);
+      }
+  });
 }
 
 // A function to submit feedback
